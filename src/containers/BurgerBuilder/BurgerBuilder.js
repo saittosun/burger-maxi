366e509740edc495772a5c8eb06b8cde801c394e
Tosun Sait <tosunsa@cronos.be>
2020-02-17 21:12:27 +0100

    implementation of the burger builder container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+// jshint esversion: 6
+import React, { Component } from "react";
+import Aux from '../../hoc/_Aux';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
+}
+
+export default BurgerBuilder;
