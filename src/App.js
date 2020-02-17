332573eb5e63404a54c1de4098bbacef4572f7ae
Tosun Sait <tosunsa@cronos.be>
2020-02-17 20:56:07 +0100

    layout component added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// jshint esversion:6
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <p>test</p>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
