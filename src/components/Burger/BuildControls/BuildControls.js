2305b82afb39f2183eb580cb73ae04abf7a4d419
Tosun Sait <tosunsa@cronos.be>
2020-02-18 11:03:02 +0100

    adding the build control component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+// jshint esversion: 6
+import React from 'react';
+
+const buildControls = (props) => (
+  <div>
+    
+  </div>
+);
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,18 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/_Aux';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     )
