4491d5a948871fe7495782097bac3112c1beeb71
Tosun Sait <tosunsa@cronos.be>
2020-02-18 11:45:05 +0100

    connecting state to build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,7 +7,9 @@ const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>less</button>
-    <button className={classes.More}>more</button>
+    <button 
+      className={classes.More} 
+      onClick={props.added}>more</button>
   </div>
 );
 
