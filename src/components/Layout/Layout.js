7cc2155d9a8e9aed12ae20710b00745a6d37ccb6
Tosun Sait <tosunsa@cronos.be>
2020-02-19 10:38:26 +0100

    added toggle button and hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,15 +11,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 //two solutions to that, do you remember them? Well for one, we could return an array here instead of JSX which is sitting next to each other, if we return an array and give each item a key, we are allowed to kind of return adjacent elements. The alternative is to create such an auxiliary higher order component. It serves only one purpose, wrapping something and immediately outputting it but hence fulfilling the requirement of having a wrapping component. we also have of course the third option of wrapping everything in a div here or another element but I don't need that div or any other element, actually I want to have adjacent elements, that is why I will go with the higher order component approach and create such a utility auxiliary component.
 class Layout extends Component {//in my opinion, it makes more sense to turn the layout component into a class component where we can implement the method so that we can listen to both the sideDrawer closing itself by clicking on the backdrop as well as toolbar opening the sideDrawer by clicking on that toggle button.
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
   sideDrawerClosedHandler = () => {
     this.setState({showSideDrawer: false})
   }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {//This is the clean way of setting the state when it depends on the old state. Now we have a secure way of toggling that and changing showSideDrawer
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      }
+    })
+  }
+
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer 
           open={this.state.showSideDrawer} 
           closed={this.sideDrawerClosedHandler}/>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,12 +4,13 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../../components/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 
 const toolbar = (props) => (
   // I do have that logo class defined in three different files but due to css modules, this is actually converted to three different css classes so that they don't interfere.
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     {/* <Logo height="80%" /> */}
     <div className={classes.Logo}>
       <Logo />
