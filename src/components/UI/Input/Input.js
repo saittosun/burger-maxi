9bf5eeb559195639c409492c1e3f1b0c439c7a5c
tostos <saittosun2@gmail.com>
2020-04-15 09:11:26 +0200

    setting up a js config for the form, handling user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,15 +5,43 @@ import classes from './Input.css';
 
 const input = (props) => {
   let inputElement = null;
-  switch (props.inputtype) {
+  switch (props.elementType) {
     case ('input'):
-      inputElement = <input className={classes.inputElement} {...props}/>
+      inputElement = <input 
+        className={classes.inputElement} 
+        {...props.elementConfig} 
+        value={props.value}
+        onChange={props.changed}/>
       break;
     case ('textarea'):
-      inputElement = <textarea className={classes.inputElement} {...props}/>
+      inputElement = <textarea 
+        className={classes.inputElement} 
+        {...props.elementConfig} 
+        value={props.value}
+        onChange={props.changed}/>
+      break;
+    case ('select'):
+      inputElement = (
+        <select
+          className={classes.inputElement}
+          value={props.value}
+          onChange={props.changed}>
+          {props.elementConfig.options.map(option => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.displayValue}
+              </option>
+            )
+          })}
+        </select> 
+      )
       break;
     default:
-      inputElement = <input className={classes.inputElement} {...props}/>
+      inputElement = <input 
+        className={classes.inputElement} 
+        {...props.elementConfig} 
+        value={props.value}
+        onChange={props.changed}/>
   }
   return (
     <div className={classes.Input}>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,11 +9,63 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    // name: '',
+    // email: '',
+    // address: {
+    //   street: '',
+    //   postalCode: ''
+    // },
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'your name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'street'
+        },
+        value: ''
+      },
+      zipcode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'your E-mail'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'}
+          ]
+        },
+        value: ''
+      }
     },
     loading: false
   }
@@ -26,16 +78,16 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: 'maxi',
-        adress: {
-          street: 'teststreet',
-          zipcode: '1111',
-          country: 'Belgium'
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest'
+      // customer: {
+      //   name: 'maxi',
+      //   adress: {
+      //     street: 'teststreet',
+      //     zipcode: '1111',
+      //     country: 'Belgium'
+      //   },
+      //   email: 'test@test.com',
+      // },
+      // deliveryMethod: 'fastest'
     }
     axios
       .post('/orders.json', order)
@@ -45,13 +97,50 @@ class ContactData extends Component {
       })
       .catch(error => this.setState({loading: false}))
   }
+
+  // I expect to get an event object as it will automatically be passed to me by react if this method is attached to an event listener which it of course is.
+  // we also receive or need a second argument which is the input identifier so that we can reach out to our state, get the right element here, the right object and adjust its value.
+  inputChangedHandler = (e, inputIdentifer) => {
+    console.log(e.target.value)
+    // this ID which is just a key from our object and that's exactly what I need. These keys here in our state object (name, street, zipcode and so on), these are the identifiers of the inputs and exactly the objects I need to adjust it. Now of course, I can use that information to update the value, the problem just is I of course can't access this.state.orderForm identifier and update the value, this is not how we mutate the state. Instead we have to mutate it, well immutably and we do this with set state.
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+
+    // I'll create a new object and used to spread operator here to create a clone. Now I can safely change the value of the updatedFormElement because it is again a clone.
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifer]
+    }
+    updatedFormElement.value = e.target.value;
+    updatedOrderForm[inputIdentifer] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
+  }
   render() {
+    const formElementsArray = [];
+    // the keys are going to be name, street, zipcode and so on and if we access order form for a given key, we get these values here on the right side of course.
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        //  this is now the right side of this property so this javascript object, that is what we we'll store in this config property.
+        config: this.state.orderForm[key]
+      })
+    }
     let form = (
       <form>
-          <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
+          {/* <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
           <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
           <Input inputtype="input" type="text" name="street" placeholder="Your street"/>
-          <Input inputtype="input" type="text" name="postal" placeholder="Your postal"/>
+          <Input inputtype="input" type="text" name="postal" placeholder="Your postal"/> */}
+          {formElementsArray.map(formElement => {
+            return (
+              <Input
+                key={formElement.id} 
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                changed={(e) => this.inputChangedHandler(e, formElement.id)}/>
+            )
+          })}
           <Button 
             btnType="Success"
             clicked={this.orderHandler}>ORDER</Button>
