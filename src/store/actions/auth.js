c38b898b4a8c061b29f12459eb566f963367049c
tostos <saittosun2@gmail.com>
2020-04-22 12:36:41 +0200

    logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,24 @@ export const authFail = (error) => {
   }
 }
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
+// with that, we got this timer in place in our action creator's file in checkAuthTimeout which will fire after whatever firebase returns us and which will then ensure that the user is logged out. And for now, this will clear the store and not do much else, it won't show any visual feedback but we will implement something to happen when the user logs out including when he deliberately logs out to give the user a clue that he's not logged in anymore.
+export const checkAuthTimeout = (expirationTime) => {
+  // I will return this function where I get dispatch as an argument because I want to run some async code.
+  return dispatch => {
+    setTimeout(() => {
+      // So here I would call dispatch after the expiration time to call the logout action, always execute these functions because that then returns the action (yukaridaki logout actioncreator dan bahsediyor) which is actually dispatched
+      dispatch(logout());
+      // I want to multiply this value with one thousand to turn my milliseconds to real seconds, so now it should be one hour.
+    }, expirationTime * 1000);
+  }
+}
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
@@ -41,7 +59,9 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(response => {
         console.log(response);
-        dispatch(authSuccess(response.data.idToken, response.data.localId))
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        // I will dispatch this checkAuthTimeout action when we get back a success response
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
         console.log(err);
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -30,11 +30,21 @@ const authFail = (state, action) => {
   });
 }
 
+// we execute or we dispatch the logout function after that time span and that allows me to now go into my reducer and there, handle the logout case.
+const authLogout = (state, action) => {
+  return updateObject(state, {
+    //  so that I am essentially logged out because all that core information that made up a logged in user is now lost again. 
+    token: null,
+    userId: null
+  })
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
     default:
       return state;
   }
